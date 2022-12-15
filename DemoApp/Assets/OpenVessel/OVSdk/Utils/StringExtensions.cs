a3b186adc4cca7d144e732dced2e1d99c55c1d54
Rishat Shamsutdinov <rishat.sh91@gmail.com>
2022-12-15 18:09:39 +0500

    commit missed files

---
+++ DemoApp/Assets/OpenVessel/OVSdk/Utils/StringExtensions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+
+namespace OVSdk.Utils
+{
+
+    public static class StringExtensions
+    {
+        public static string Repeat(this string s, int n)
+            => new StringBuilder(s.Length * n).Insert(0, s, n).ToString();
+    }
+
+}
