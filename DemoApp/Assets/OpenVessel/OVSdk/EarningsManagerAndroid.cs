81b0268ead413d4fb4f9cd52ed2b566759874071
Rishat Shamsutdinov <rishat.sh91@gmail.com>
2023-02-14 23:13:04 +0500

    add Android stubs for earnings

---
+++ DemoApp/Assets/OpenVessel/OVSdk/EarningsManagerAndroid.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using Logger = OVSdk.Utils.Logger;
+
+namespace OVSdk
+{
+#if UNITY_ANDROID
+    public class EarningsManagerAndroid: EarningsManagerBase
+    {
+
+        public void TrackRevenuedAd(AdType adType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowEarnings(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+#endif
+}
